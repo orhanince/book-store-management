036f964d73fd39270d44cc8cbee2f7724bb96482
orhanince <ince.orhan@yandex.com>
2024-02-15 16:40:04 +0300

    Add typeorm and configure postgresql database connection with typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfiguration from './config/app.configuration';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PrimaryDataBaseConfig } from './common/database/typeorm/typeorm.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfiguration],
+    }),
+    TypeOrmModule.forRoot(PrimaryDataBaseConfig()),
+    EventEmitterModule.forRoot(),
+  ],
+  providers: [Logger],
 })
 export class AppModule {}
---
+++ src/config/app.configuration.ts
@@ -0,0 +1,27 @@
+function appConfig() {
+  return {
+    APP_NAME: process.env.APP_NAME || 'APP',
+    APP_ENV: process.env.APP_ENV || 'production',
+    APP_DEBUG: process.env.APP_DEBUG === 'true',
+    APP_DEFAULT_LANGUAGE: process.env.APP_DEFAULT_LANGUAGE || 'en',
+
+    HTTP_PORT: parseInt(process.env.HTTP_PORT, 10) || 3000,
+    HTTP_HOST: process.env.HTTP_HOST || '0.0.0.0',
+
+    DATABASE_PROVIDER: process.env.DATABASE_PROVIDER || 'postgres',
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_PORT: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    DATABASE_USE_SSL: process.env.DATABASE_USE_SSL === 'true',
+
+    LOG_CHANNELS: process.env.LOG_CHANNELS || 'console,file',
+    LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
+    LOG_FILE_NAME: process.env.LOG_FILE_NAME || 'nest.log',
+    LOG_FILE_DIRNAME: process.env.LOG_FILE_DIRNAME || 'logs',
+  };
+}
+
+export type AppConfig = ReturnType<typeof appConfig>;
+export default appConfig;
