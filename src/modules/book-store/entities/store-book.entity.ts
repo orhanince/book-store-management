5f09ddd6c4a75ac415ce58ee0297f0a492b7a976
orhanince <ince.orhan@yandex.com>
2024-02-18 15:10:36 +0300

    Add store book and store_books table.

---
+++ src/modules/book-store/entities/store-book.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn
+} from 'typeorm';
+import BookStore from './book-store.entity';
+
+@Entity({ name: 'store_books' })
+export default class StoreBook {
+  @PrimaryGeneratedColumn({ type: 'bigint', name: 'id', unsigned: true })
+  public ID: bigint;
+
+  @Column({ type: 'bigint', nullable: false, name: 'book_id' })
+  public bookID: bigint;
+
+  @Column({ type: 'bigint', nullable: false, name: 'book_store_id' })
+  public bookStoreID: bigint;
+
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  updatedAt: Date;
+
+  @ManyToOne(() => BookStore, (target) => target.store_books)
+  @JoinColumn({ name: 'book_store_id' })
+  store_book: BookStore;
+}
