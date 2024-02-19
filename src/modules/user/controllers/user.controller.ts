55b1a00d8713d08051a7f10612564ecf74dc76ee
orhanince <ince.orhan@yandex.com>
2024-02-19 14:09:07 +0300

    Update user module and add addUser method

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,9 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/modules/auth/guards/auth.guard';
+import { RolesGuard } from 'src/modules/auth/guards/role.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { AddUserRequest, AddUserResponse, UserListResponse } from '../dtos';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles('admin')
+  @Get('/')
+  async getUsers(): Promise<UserListResponse> {
+    return await this.userService.getUsers();
+  }
+
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles('admin')
+  @Post('/')
+  async addUser(
+    @Body() addUserRequest: AddUserRequest
+  ): Promise<AddUserResponse> {
+    return await this.userService.addUser(addUserRequest);
+  }
 }
--- src/modules/user/dtos/index.ts
+++ src/modules/user/dtos/index.ts
@@ -1,10 +1,12 @@
 import { CheckUserRequest, CheckUserResponse } from './check-user.dto';
 import { UserListResponse } from './user-list.dto';
 import { GetUserResponse } from './get-user.dto';
-
+import { AddUserRequest, AddUserResponse } from './add-user-dto';
 export {
   CheckUserRequest,
   CheckUserResponse,
   GetUserResponse,
-  UserListResponse
+  UserListResponse,
+  AddUserRequest,
+  AddUserResponse
 };
