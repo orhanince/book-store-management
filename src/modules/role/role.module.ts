dd7eb76654371a42b953bb72790e814480c1df2b
orhanince <ince.orhan@yandex.com>
2024-02-19 11:15:56 +0300

    Update role module with auth guard and roles decorator

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
-import { AdminRoleController } from './controllers/admin-role.controller';
+import { RoleController } from './controllers/role.controller';
 import { RoleRepository } from './repository/role.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Role from './entities/role.entity';
 import UserRole from './entities/user-role.entity';
 import { RoleService } from './services/role.service';
+import { AuthModule } from '../auth/auth.module';
 @Module({
-  imports: [CacheModule.register(), TypeOrmModule.forFeature([UserRole, Role])],
-  controllers: [AdminRoleController],
+  imports: [
+    CacheModule.register(),
+    TypeOrmModule.forFeature([UserRole, Role]),
+    forwardRef(() => AuthModule)
+  ],
+  controllers: [RoleController],
   providers: [RoleRepository, RoleService],
   exports: [RoleRepository, RoleService]
 })
